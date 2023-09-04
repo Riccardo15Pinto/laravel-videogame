de9f09dc713db1076c9dab4ef9cd838c5e522c02
Tommicavo <tommicava@icloud.com>
2023-09-04 16:08:39 +0200

    table of videogames in index.blade.php

--- resources/views/admin/videogames/index.blade.php
+++ resources/views/admin/videogames/index.blade.php
@@ -12,31 +12,39 @@
         </div>
     </header>
 
-    <div class="row">
-        @foreach ($videogames as $videogame)
-            <div class="col-md-4">
-                <div class="card mb-4">
-                    <img src="{{ $videogame->img_path }}" class="card-img-top" alt="{{ $videogame->title }}">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $videogame->title }}</h5>
-                        <p class="card-text">{{ $videogame->description }}</p>
-                    </div>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item"><strong>Data di uscita:</strong> {{ $videogame->release_date }}</li>
-                        <li class="list-group-item"><strong>Età minima:</strong> {{ $videogame->min_age }} anni</li>
-                    </ul>
-                    <div class="card-footer d-flex">
-                        <a href="{{ route('admin.videogames.show', $videogame) }}" class="btn btn-primary ms-2">Go to
-                            the videogame</a>
-                        <a href="{{ route('admin.videogames.edit', $videogame) }}" class="btn btn-warning ms-2">Edit</a>
-                        <button type="button" class="btn btn-danger ms-2" data-bs-toggle="modal"
-                            data-bs-target="#{{ $videogame->id }}">
-                            Delete
-                        </button>
-                        @include('admin.includes.modal_delete')
-                    </div>
-                </div>
-            </div>
-        @endforeach
+    <div class="indexContent">
+        <table class="table mb-4">
+            <thead>
+                <tr>
+                    <th scope="col" colspan="1" width="10%">Id</th>
+                    <th scope="col" colspan="1" width="60%">Title</th>
+                    <th class="text-center" scope="col" colspan="1" width="30%">Tasks</th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse($videogames as $videogame)
+                    <tr>
+                        <td>{{ $videogame->id }}</td>
+                        <td>{{ $videogame->title }}</td>
+                        <td>
+                            <div class="d-flex justify-content-center align-items-center gap-3">
+                                <a href="{{ route('admin.videogames.show', $videogame) }}" class="btn btn-primary ms-2">Go
+                                    to
+                                    the videogame</a>
+                                <a href="{{ route('admin.videogames.edit', $videogame) }}"
+                                    class="btn btn-warning ms-2">Edit</a>
+                                <button type="button" class="btn btn-danger ms-2" data-bs-toggle="modal"
+                                    data-bs-target="#{{ $videogame->id }}">
+                                    Delete
+                                </button>
+                                @include('admin.includes.modal_delete')
+                            </div>
+                        </td>
+                    </tr>
+                @empty
+                    <h3>No Videogames available at this moment...</h3>
+                @endforelse
+            </tbody>
+        </table>
     </div>
 @endsection
