366dfa9f3712744f2a23ccc1961346de440650e3
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-09-04 16:46:16 +0200

    fix show admin

--- resources/views/admin/videogames/show.blade.php
+++ resources/views/admin/videogames/show.blade.php
@@ -9,38 +9,38 @@
         </div>
         <div class="d-flex justify-content-end my-3">
             <a href="{{ route('admin.videogames.create') }}" class="btn btn-success ms-2">Create new videogame</a>
+            <a href="{{ route('admin.videogames.index') }}" class="btn btn-primary ms-2">Torna alla Home</a>
         </div>
     </header>
 
-    <div class="row">
+    <div class="container my-4">
 
-        <div class="col-md-4">
-            <div class="card mb-4">
-                <img src="{{ $videogame->img_path }}" class="card-img-top" alt="{{ $videogame->title }}">
-                <div class="card-body">
-                    <h5 class="card-title">{{ $videogame->title }}</h5>
-                    <p class="card-text">{{ $videogame->description }}</p>
+        <div class="card mb-3" style="max-width: 540px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="{{ $videogame->img_path }}" class="card-img-top" alt="{{ $videogame->title }}">
                 </div>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item"><strong>Data di uscita:</strong> {{ $videogame->release_date }}</li>
-                    <li class="list-group-item"><strong>Età minima:</strong> {{ $videogame->min_age }} anni</li>
-                </ul>
-                <div class="card-footer d-flex">
-                    <a href="{{ route('admin.videogames.edit', $videogame) }}" class="btn btn-warning ms-2">Edit</a>
-                    {{-- <form action="{{ route('admin.videogames.destroy', $videogame) }}" method="POST"
-                        class="ms-2 delete-form">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="btn btn-danger">Delete</button>
-                    </form> --}}
-                    <button type="button" class="btn btn-danger ms-2" data-bs-toggle="modal"
-                        data-bs-target="#{{ $videogame->id }}">
-                        Delete
-                    </button>
-                    @include('admin.includes.modal_delete')
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $videogame->title }}</h5>
+                        <p class="card-text">{{ $videogame->description }}</p>
+                        <span><strong>Data di uscita:</strong> {{ $videogame->release_date }}</span>
+                        <span><strong>Età minima:</strong> + {{ $videogame->min_age }}</span>
+                        <div class="d-flex align-items-center justify-content-end">
+
+                            <a href="{{ route('admin.videogames.edit', $videogame) }}" class="btn btn-warning">Edit</a>
+
+                            <button type="button" class="btn btn-danger ms-2" data-bs-toggle="modal"
+                                data-bs-target="#{{ $videogame->id }}">
+                                Delete
+                            </button>
+                            @include('admin.includes.modal_delete')
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
 
     </div>
+
 @endsection
