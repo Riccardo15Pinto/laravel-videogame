72975bb52624c189ff2046e01a9a2cef7d1a5dae
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-09-04 12:25:09 +0200

    set edit and update admin controller , and create view admin/edit.blade.php

--- app/Http/Controllers/Admin/VideogameController.php
+++ app/Http/Controllers/Admin/VideogameController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Models\Videogame;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Validation\Rule;
 
 class VideogameController extends Controller
 {
@@ -45,7 +46,7 @@ class VideogameController extends Controller
      */
     public function edit(Videogame $videogame)
     {
-        //
+        return view('admin.update', $videogame);
     }
 
     /**
@@ -53,7 +54,33 @@ class VideogameController extends Controller
      */
     public function update(Request $request, Videogame $videogame)
     {
-        //
+        $data_new_videogame = $request->all();
+
+        $request->validate(
+            [
+                'title' => ['required', 'string', 'max:50', Rule::unique('videogames')->ignore($videogame)],
+                'description' => 'required|string|',
+                'img_path' => 'required|url:http,https',
+                'release_date' => 'required|string',
+                'min_age' => 'required|string|min:6|max:18',
+            ],
+            [
+                'title.required' => 'Il Nome del videogioco è obbligatorio',
+                'title.unique' => 'Il Nome del videogioco esiste già',
+                'img_path.required' => 'L\'url è obbligatorio',
+                'img_path.url' => 'L\'url deve contenere http , https',
+                'description.required' => 'La descrizione è obbligatoria',
+                'release_date.required' => 'La data di uscita è obbligatoria',
+                'min_age.required' => 'L\' età consigliata è obbligatoria',
+                'min_age.min' => 'L\' età consigliata deve essere minimo 6',
+                'min_age.max' => 'L\' età consigliata deve essere massimo 18',
+            ]
+        );
+
+
+        $videogame->update($data_new_videogame);
+
+        return to_route('admin.show', $videogame)->with('alert-type', 'success')->with('alert-message', "$videogame->title modificato con successo");
     }
 
     /**
---
+++ resources/views/admin/edit.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h2 class="fs-4 text-secondary my-4">
+            <h1>Form di edit</h1>
+        </h2>
+
+    </div>
+@endsection
