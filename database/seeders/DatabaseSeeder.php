ad8a7b0c26d24f9b6b827b430ca218aeecfb0231
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-09-08 12:12:55 +0200

    add new seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,6 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        $this->call(VideogameSeeder::class);
+        $this->call([PublisherSeeder::class, VideogameSeeder::class]);
     }
 }
