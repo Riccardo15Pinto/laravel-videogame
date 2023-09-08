5a457f02d37cc4551f0093320b75c7b2796c6c25
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-09-08 14:59:40 +0200

    create file console in config

---
+++ config/console.php
@@ -0,0 +1,32 @@
+<?php return [
+    [
+        'name_company' => 'SONY',
+        'city' => 'Japan',
+        'web_site' => '',
+        'description' => '',
+    ],
+    [
+        'name_company' => 'X-BOX',
+        'city' => 'United States of America',
+        'web_site' => '',
+        'description' => '',
+    ],
+    [
+        'name_company' => 'Nintendo',
+        'city' => 'Japan',
+        'web_site' => '',
+        'description' => '',
+    ],
+    [
+        'name_company' => 'PC',
+        'city' => 'N.D',
+        'web_site' => '',
+        'description' => '',
+    ],
+    [
+        'name_company' => 'Mobile',
+        'city' => 'Japan',
+        'web_site' => '',
+        'description' => '',
+    ],
+];
